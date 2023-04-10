7b92e28eab5b654ad4d1e07515757b38adf91d9f
Dboee <davidboee@gmail.com>
2023-04-10 13:54:23 +0200

    adds more examples and tests

--- src/basicCall.ts
+++ src/basicCall.ts
@@ -1,9 +1,12 @@
-import { OpenAI } from 'langchain/llms';
+const { OpenAI } = require('langchain');
+require('dotenv').config();
 
-export const run = async () => {
+const basicCall = async () => {
   // temerature controls how ransom/creative the response is. (range: 0-1)
   const model = new OpenAI({ temperature: 0.5 });
 
   const res = await model.call('What is the best way to get rich?');
   console.log(res);
 };
+
+export default basicCall;
---
+++ src/index.js
@@ -0,0 +1,13 @@
+"use strict";
+const { PromptTemplate } = require('langchain');
+require('dotenv').config();
+const run = async () => {
+    const template = 'What language do they spreak in {country}?';
+    const prompt = new PromptTemplate({
+        inputVariables: ['country'],
+        template,
+    });
+    const res = await prompt.format({ country: 'France' });
+    console.log(res);
+};
+run();
